3d8856988f25ee1fd69195f466249070cbf32228
yanzaiyun43 <132528785+yanzaiyun43@users.noreply.github.com>
2025-12-31 13:15:57 +0800

    Update Footer.vue

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -3,7 +3,8 @@
     <main class="m-auto pt-4 flex flex-col-reverse md:flex-row justify-between gap-2 md:gap-16 w-full max-w-[1666px] border-t">
       
     </main>
-    <section class="[&>p]:text-xs [&>p]:w-full [&>p]:py-[6px] [&>p]:text-center md:[&>p]:text-left">
+    <!-- 移除 md:[&>p]:text-left，确保所有屏幕尺寸下都居中 -->
+    <section class="[&>p]:text-xs [&>p]:w-full [&>p]:py-[6px] [&>p]:text-center">
         <p>海阔图床是旧识桥支持的文件上传项目，致力于为用户提供稳定的永久存储服务。</p>
         <p class="text-red-600">注意:上传违反中国大陆、香港及美国法律的图片将会直接删除，并封禁设备IP。</p>
     </section>
