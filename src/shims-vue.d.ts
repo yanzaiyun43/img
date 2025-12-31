7d6aef9b10399773436bc2fbcba3ded1008a1d73
yanzaiyun43 <132528785+yanzaiyun43@users.noreply.github.com>
2025-12-31 13:08:40 +0800

    Create shims-vue.d.ts

---
+++ src/shims-vue.d.ts
@@ -0,0 +1,7 @@
+// src/shims-vue.d.ts
+/* eslint-disable */
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
